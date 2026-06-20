38fb29b860273ba858de1ab92ce1b9924a5b86b5
Ryan2128 <ryanzhao2128@gmail.com>
2026-06-20 17:50:29 +0800

    Hide prompt helper buttons on runner node

--- js/prompt_runner.js
+++ js/prompt_runner.js
@@ -1,5 +1,45 @@
 import { app } from "../../../scripts/app.js";
 
+const PROMPT_HELPER_LABELS = [
+  "翻译",
+  "标签工具",
+  "提示词优化",
+];
+
+function isPromptHelperWidget(widgetOrName, value, options) {
+  const parts = [
+    widgetOrName?.name,
+    widgetOrName?.label,
+    widgetOrName?.value,
+    typeof widgetOrName === "string" ? widgetOrName : "",
+    value,
+    options?.label,
+  ];
+
+  return parts.some((part) => {
+    if (part == null) {
+      return false;
+    }
+
+    const text = String(part);
+    return PROMPT_HELPER_LABELS.some((label) => text.includes(label));
+  });
+}
+
+function removePromptHelperWidgets(node) {
+  if (!node?.widgets?.length) {
+    return;
+  }
+
+  const before = node.widgets.length;
+  node.widgets = node.widgets.filter((widget) => !isPromptHelperWidget(widget));
+
+  if (node.widgets.length !== before) {
+    node.setSize?.(node.computeSize?.() ?? node.size);
+    app.graph.setDirtyCanvas(true, true);
+  }
+}
+
 app.registerExtension({
   name: "OpenAICompatiblePromptRunner.OutputText",
   beforeRegisterNodeDef(nodeType, nodeData) {
@@ -7,9 +47,36 @@ app.registerExtension({
       return;
     }
 
+    const addWidget = nodeType.prototype.addWidget;
+    nodeType.prototype.addWidget = function (type, name, value, callback, options) {
+      const widget = addWidget?.apply(this, arguments);
+
+      if (isPromptHelperWidget(name, value, options)) {
+        removePromptHelperWidgets(this);
+      }
+
+      return widget;
+    };
+
+    const onNodeCreated = nodeType.prototype.onNodeCreated;
+    nodeType.prototype.onNodeCreated = function () {
+      onNodeCreated?.apply(this, arguments);
+      removePromptHelperWidgets(this);
+      setTimeout(() => removePromptHelperWidgets(this), 0);
+      setTimeout(() => removePromptHelperWidgets(this), 300);
+    };
+
+    const onConfigure = nodeType.prototype.onConfigure;
+    nodeType.prototype.onConfigure = function () {
+      onConfigure?.apply(this, arguments);
+      removePromptHelperWidgets(this);
+      setTimeout(() => removePromptHelperWidgets(this), 0);
+    };
+
     const onExecuted = nodeType.prototype.onExecuted;
     nodeType.prototype.onExecuted = function (message) {
       onExecuted?.apply(this, arguments);
+      removePromptHelperWidgets(this);
 
       if (!message?.output_text) {
         return;
